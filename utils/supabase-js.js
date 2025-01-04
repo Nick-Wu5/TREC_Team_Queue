98c2eef5d657d562adfb1219ca268952e47c4806
Nick Wu <wu2264@purdue.edu>
2025-01-04 18:27:24 -0500

    Update supabase-js.js

--- utils/supabase-js.js
+++ utils/supabase-js.js
@@ -1,10 +1,18 @@
 import { createClient } from "@supabase/supabase-js";
+require("dotenv").config({ path: ".env.local" });
 
 // Load environment variables for client-side use
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log("Email:", process.env.SUPABASE_USER_EMAIL);
+console.log("Password:", process.env.SUPABASE_USER_PASSWORD);
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 const loginUser = async () => {
   const { data, error } = await supabase.auth.signInWithPassword({
