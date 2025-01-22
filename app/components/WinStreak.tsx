4d12d53ea354b1ff5240501ea2a899dd0ddee2b7
Nick Wu <wu2264@purdue.edu>
2025-01-22 14:34:58 -0500

    Update WinStreak.tsx

--- app/components/WinStreak.tsx
+++ app/components/WinStreak.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../styles/WinStreak.css";
 import Image from "next/image";
-import { relative } from "path";
 
 /**
  * WinStreak component visually displays a team's win streak with an animated
